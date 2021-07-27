205c1f4358976ea1020c64bdade329f7140f4ace
Ondřej Hlaváč <ondrej.hlavac@outlook.com>
2021-07-27 05:13:28 +0200

    fk upasodfa

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChakraProvider, Flex, Heading, Link } from '@chakra-ui/react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import { routes } from './constants/routes';
 import DetailPage from './pages/DetailPage';
@@ -12,7 +12,7 @@ import './App.css';
 function App() {
   return (
     <ChakraProvider>
-      <Router basename="./">
+      <Router basename="/">
         {/* <Header /> */}
 
         <Flex p={{ base: 4, md: 8 }} grow={1} zIndex={99} justifyContent="center" borderBottom="1px solid #000000">
@@ -21,12 +21,14 @@ function App() {
           </Link>
         </Flex>
 
-        <Route exact path={routes.HOME}>
-          <Homepage />
-        </Route>
-        <Route path={routes.DETAIL}>
-          <DetailPage />
-        </Route>
+        <Switch>
+          <Route exact path={routes.HOME}>
+            <Homepage />
+          </Route>
+          <Route path={routes.DETAIL}>
+            <DetailPage />
+          </Route>
+        </Switch>
       </Router>
     </ChakraProvider>
   );
