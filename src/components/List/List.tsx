6ef68c70046cb4bea76615a80160f745db2a25e1
Ondřej Hlaváč <ondrej.hlavac@outlook.com>
2021-07-27 05:39:02 +0200

    12

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,13 +3,20 @@ import { SimpleGrid } from '@chakra-ui/react';
 import Thumbnail from '../Thumbnail';
 
 const List = () => {
-  var arr = new Array(12);
-
   return (
     <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10}>
-      {[...arr].map((item: any, index: number) => {
-        return <Thumbnail key={index} />;
-      })}
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
+      <Thumbnail />
     </SimpleGrid>
   );
 };
