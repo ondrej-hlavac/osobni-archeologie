f60fa8329d639993bbd191c7521d3755a4023923
Ondřej Hlaváč <ondrej.hlavac@outlook.com>
2021-08-22 22:59:43 +0200

    update api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,7 +2,7 @@ export const baseApiUrl =
   // eslint-disable-next-line no-undef
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3030/.netlify/functions/server'
-    : 'https://vigilant-spence-429f6a.netlify.app/.netlify/functions/server';
+    : 'https://api-osobniarcheologie.netlify.app/.netlify/functions/server';
 
 export const FINDINGS = `${baseApiUrl}/findings`;
 export const TAGS = `${baseApiUrl}/tags`;
