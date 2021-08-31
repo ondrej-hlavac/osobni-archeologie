d81d8f817306a024defb0b88df28087cf998e9ad
Ondřej Hlaváč <ondrej.hlavac@outlook.com>
2021-09-01 00:03:43 +0200

    fixed tags

--- src/components/TagsList/TagsList.tsx
+++ src/components/TagsList/TagsList.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Flex, Heading, Wrap } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 import Tag from '../Tag';
 import { routes } from '../../constants/routes';
 // import { routes } from '../../constants/routes';
 
 const TagsList = ({ tags, headline, link = false }: any) => {
   if (!tags) {
-    console.log('return null');
     return null;
   }
 
@@ -17,13 +16,13 @@ const TagsList = ({ tags, headline, link = false }: any) => {
           {headline}
         </Heading>
       )}
-      <Flex my={8} alignItems="flex-start" width="100%" textAlign="left">
+      <Flex flexWrap="wrap" my={8} alignItems="flex-start" width="100%" textAlign="left">
         {tags.data?.map((tag: any, index: number) => {
           return (
             <Tag
               color={`#${tag.data.color}`}
               link={link ? `${routes.HOME}?tag=${tag?.ref?.['@ref']?.id}` : undefined}
-              key={`key_${tag.data.name}${index}`}
+              key={`key_${tag.data.name}${index.toString()}`}
               label={tag.data.name}
             />
           );
