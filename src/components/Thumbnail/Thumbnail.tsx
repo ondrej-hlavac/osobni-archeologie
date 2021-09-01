711f2a5a927ad6c4dccd65d752f02793e3ce6348
Ondřej Hlaváč <ondrej.hlavac@outlook.com>
2021-09-01 03:25:27 +0200

    subpage filtering

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -11,11 +11,11 @@ interface IProps {
 }
 
 const Thumbnail = ({ imageUrl, basicTag, timeTag, findingData }: IProps) => {
-  // const { data } = findingData;
-  // console.log('🚀 ~ file: Thumbnail.tsx ~ line 15 ~ Thumbnail ~ data', data);
+  const { data } = findingData;
 
   return (
     <Box border="1px solid rgba(0,0,0,0.4)" p={0} position="relative">
+      {console.log('🚀 ~ file: Thumbnail.tsx ~ line 30 ~ Thumbnail ~ basicTag', basicTag)}
       <Link
         href={`${routes.DETAIL}/${findingData.ref?.['@ref'].id}`}
         position="relative"
@@ -26,8 +26,8 @@ const Thumbnail = ({ imageUrl, basicTag, timeTag, findingData }: IProps) => {
         <Image position="absolute" width="100%" src={imageUrl} objectFit="contain" objectPosition="50% 50%" alt="placeholder" m="auto" />
       </Link>
       <Flex padding={2} position="absolute" bottom="0" left="0" right="0" backgroundColor="#ffffff83" zIndex={99}>
-        <Tag color={`#${basicTag.color}`} label={basicTag.name} />
-        <Tag color={`#${timeTag.color}`} label={timeTag.name} />
+        <Tag color={`#${basicTag.color}`} label={basicTag.name} link={`${routes.HOME}?tag=${data.basicTagId}`} />
+        <Tag color={`#${timeTag.color}`} label={timeTag.name} link={`${routes.HOME}?tag=${data.timeTagId}`} />
       </Flex>
     </Box>
   );
