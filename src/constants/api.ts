2b5a10e4bdd5a5ed9c81badc50a1c856b802f684
Ondřej <ondrej.hlavac@outlook.com>
2021-09-23 14:32:54 +0200

    bla

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,7 +2,7 @@ export const baseApiUrl =
   // eslint-disable-next-line no-undef
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3030/.netlify/functions/server'
-    : 'https://api-osobniarcheologie.netlify.app/.netlify/functions/server';
+    : 'https://api-osobniarcheologie.com/.netlify/functions/server';
 
 export const FINDINGS = `${baseApiUrl}/findings`;
 export const TAGS = `${baseApiUrl}/tags`;
